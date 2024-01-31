6cad26ed655e3b0f4033a95f82ca66f295049ef0
BiusMichaelJoseph <85612158+BiusMichaelJoseph@users.noreply.github.com>
2024-01-31 06:57:01 +0300

    Add files via upload

---
+++ myHustle/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('userAuth.urls')),
+    path('api/profile/', include('userProfile.urls')),
+    path('api/mediaInteractions/', include('mediaInteractions.urls')),
+    path('api/wallet/', include('wallet.urls'))
+]
